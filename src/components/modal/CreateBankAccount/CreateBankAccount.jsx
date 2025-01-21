0ae8700036312d6db72a3b20ebab8f0ec08ecfdb
sabbirHossain <sabbirshnt@gmail.com>
2025-01-21 15:19:09 +0600

    done

--- src/components/modal/CreateBankAccount/CreateBankAccount.jsx
+++ src/components/modal/CreateBankAccount/CreateBankAccount.jsx
@@ -2,6 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import useCloseModalClickOutside from "../../../hooks/useCloseModalClickOutside";
 import toast from "react-hot-toast";
 import { useBankMutation } from "../../../redux/features/payment/payment.api";
+import { useSelector } from "react-redux";
+import { jwtDecode } from "jwt-decode";
+import { AxiosSecure } from "../../../lib/AxiosSecure";
+import { API } from "../../../api";
 
 const CreateBankAccount = ({ refetchBankAccounts, setShowAddBank }) => {
   const addBankRef = useRef();
@@ -13,7 +17,12 @@ const CreateBankAccount = ({ refetchBankAccounts, setShowAddBank }) => {
     accountNumber: "",
     confirmAccountNumber: "",
     upiId: "",
+    otp: "",
   });
+  const [mobile, setMobile] = useState(null);
+  const { token } = useSelector((state) => state.auth);
+  const [orderId, setOrderId] = useState(null);
+  const [timer, setTimer] = useState(null);
 
   useCloseModalClickOutside(addBankRef, () => {
     setShowAddBank(false);
@@ -25,15 +34,24 @@ const CreateBankAccount = ({ refetchBankAccounts, setShowAddBank }) => {
     if (bankDetails.accountNumber !== bankDetails.confirmAccountNumber) {
       return toast.error("Account number not matched!");
     }
+
+    if (mobile && !bankDetails.otp) {
+      return toast.error("Please enter otp to add new account");
+    }
     /* generating random token for post data */
 
-    const bankData = {
+    let bankData = {
       accountName: bankDetails.accountName,
       ifsc: bankDetails.ifsc,
-      upiId: bankDetails.upiId,
       accountNumber: bankDetails.accountNumber,
+      upiId: bankDetails.upiId,
       type: "addBankAccount",
     };
+    if (mobile) {
+      bankData.mobile = mobile;
+      bankData.otp = bankDetails.otp;
+      bankData.orderId = orderId;
+    }
 
     const res = await addNewBank(bankData).unwrap();
 
@@ -43,6 +61,7 @@ const CreateBankAccount = ({ refetchBankAccounts, setShowAddBank }) => {
         ifsc: "",
         accountNumber: "",
         confirmAccountNumber: "",
+        otp: "",
       });
       toast.success(res?.result?.message);
       setShowAddBank(false);
@@ -56,14 +75,55 @@ const CreateBankAccount = ({ refetchBankAccounts, setShowAddBank }) => {
     const isaccountNameFilled = bankDetails.accountName.trim() !== "";
     const isaccountNumberFilled = bankDetails.accountNumber.trim() !== "";
     const isIfscFilled = bankDetails.ifsc.trim() !== "";
+    const isOTPFilled = mobile ? bankDetails.otp.trim() !== "" : true;
     const isFormValid =
-      isaccountNameFilled && isIfscFilled && isaccountNumberFilled;
+      isaccountNameFilled &&
+      isIfscFilled &&
+      isaccountNumberFilled &&
+      isOTPFilled;
     setIsFormValid(isFormValid);
   };
 
   useEffect(() => {
     validateForm(bankDetails);
   }, [bankDetails]);
+
+  const getOtp = async () => {
+    const otpData = {
+      mobile,
+    };
+
+    const res = await AxiosSecure.post(API.otp, otpData);
+    const data = res.data;
+    if (data?.success) {
+      setTimer(60);
+      setOrderId(data?.result?.orderId);
+      toast.success(data?.result?.message);
+    } else {
+      toast.error(data?.error?.errorMessage);
+    }
+  };
+
+  useEffect(() => {
+    const getMobile = () => {
+      const decode = jwtDecode(token);
+      if (decode?.mobile) {
+        setMobile(decode?.mobile);
+      }
+    };
+    getMobile();
+  }, [token]);
+
+  useEffect(() => {
+    if (timer > 0) {
+      setTimeout(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+    } else {
+      setTimer(null);
+    }
+  }, [timer]);
+
   return (
     <div
       id="popup-modal"
@@ -140,24 +200,25 @@ const CreateBankAccount = ({ refetchBankAccounts, setShowAddBank }) => {
                   className="w-full font-lato uppercase"
                 >
                   <div className="text-[10px] ml-1 md:text-xs lg:text-sm">
-                    IFSC Code
+                    Account Name
                   </div>
                   <div className="flex w-full items-center py-2 px-2 bg-auth rounded-lg border">
                     <input
                       onChange={(e) => {
                         setBankDetails({
                           ...bankDetails,
-                          ifsc: e.target.value,
+                          accountName: e.target.value,
                         });
                       }}
-                      placeholder="Enter IFSC Code"
+                      placeholder="Enter Account Name"
                       id="password-input"
                       className="block w-full focus:outline-none w-full pr-2 rounded-none text-text_Ternary bg-auth text-sm xs:text-md"
-                      value={bankDetails.ifsc}
+                      value={bankDetails.accountName}
                     />
                   </div>
                 </div>
               </div>
+
               <div className="flex flex-col gap-1">
                 <div
                   title="passwordInput"
@@ -212,24 +273,101 @@ const CreateBankAccount = ({ refetchBankAccounts, setShowAddBank }) => {
                   className="w-full font-lato uppercase"
                 >
                   <div className="text-[10px] ml-1 md:text-xs lg:text-sm">
-                    Account Name
+                    IFSC Code
                   </div>
                   <div className="flex w-full items-center py-2 px-2 bg-auth rounded-lg border">
                     <input
                       onChange={(e) => {
                         setBankDetails({
                           ...bankDetails,
-                          accountName: e.target.value,
+                          ifsc: e.target.value,
                         });
                       }}
-                      placeholder="Enter Account Name"
+                      placeholder="Enter IFSC Code"
                       id="password-input"
                       className="block w-full focus:outline-none w-full pr-2 rounded-none text-text_Ternary bg-auth text-sm xs:text-md"
-                      value={bankDetails.accountName}
+                      value={bankDetails.ifsc}
                     />
                   </div>
                 </div>
               </div>
+              {mobile && (
+                <div className="flex flex-col gap-1">
+                  <div
+                    title="passwordInput"
+                    className="w-full font-lato uppercase"
+                  >
+                    <div className="text-[10px] ml-1 md:text-xs lg:text-sm">
+                      Mobile
+                    </div>
+                    <div className="flex w-full items-center py-2 bg-auth rounded-lg border">
+                      {/* <span
+               id="dropdown-phone-button"
+               className="flex-shrink-0 z-10 inline-flex items-center pl-2 pr-1 text-sm sm:text-md font-normal text-center"
+             >
+               +91
+             </span> */}
+                      <input
+                        readOnly
+                        id="mobile-no-input"
+                        className="px-2 block w-full focus:outline-none w-full font-lato bg-auth rounded-none text-text_Ternary pr-2 text-sm xs:text-md"
+                        placeholder="Phone Number"
+                        type="text"
+                        value={mobile}
+                      />
+
+                      <div className="w-max">
+                        {timer ? (
+                          <button
+                            onClick={getOtp}
+                            className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out font-lato-bold h-fit bg-bg_Primary text-text_Quaternary transition-all ease-in-out text-xs whitespace-nowrap mr-1 py-1 px-3 rounded active:scale-[0.98] active:opacity-95 disabled:bg-bg_Slate500 disabled:opacity-50 font-medium relative flex items-center justify-center cursor-pointer"
+                            type="button"
+                          >
+                            <span className=" ">Retry in {timer}</span>
+                            {/* <span className="shimmer"></span> */}
+                          </button>
+                        ) : (
+                          <button
+                            onClick={getOtp}
+                            className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out font-lato-bold h-fit bg-bg_Primary text-text_Quaternary transition-all ease-in-out text-xs whitespace-nowrap mr-1 py-1 px-3 rounded active:scale-[0.98] active:opacity-95 disabled:bg-bg_Slate500 disabled:opacity-50 font-medium relative flex items-center justify-center cursor-pointer"
+                            type="button"
+                          >
+                            <span className=" ">Get OTP</span>
+                            <span className="shimmer"></span>
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {mobile && (
+                <div className="flex flex-col gap-1">
+                  <div
+                    title="passwordInput"
+                    className="w-full font-lato uppercase"
+                  >
+                    <div className="text-[10px] ml-1 md:text-xs lg:text-sm">
+                      OTP
+                    </div>
+                    <div className="flex w-full items-center border p-1 bg-auth rounded-lg mt-2">
+                      <input
+                        onChange={(e) => {
+                          setBankDetails({
+                            ...bankDetails,
+                            otp: e.target.value,
+                          });
+                        }}
+                        id="otpSignUp"
+                        className="block w-full focus:outline-none w-full font-lato rounded-none py-1 text-text_Ternary px-2 text-sm xs:text-md bg-auth"
+                        placeholder="Enter OTP"
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
+
               <div id="googleRecaptcha" className="hidden"></div>
               <div title="loginButton" className="w-full">
                 <button
