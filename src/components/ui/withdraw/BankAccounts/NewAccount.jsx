54c148d99d88cb9eb40645d17e1ccad9ed2b10da
sabbirHossain <sabbirshnt@gmail.com>
2025-01-22 10:16:47 +0600

    done

--- src/components/ui/withdraw/BankAccounts/NewAccount.jsx
+++ src/components/ui/withdraw/BankAccounts/NewAccount.jsx
@@ -2,17 +2,26 @@ import { useEffect, useState } from "react";
 import { useBankMutation } from "../../../../redux/features/payment/payment.api";
 import toast from "react-hot-toast";
 import useGetAllBankAccount from "../../../../hooks/useGetAllBankAccount";
+import { jwtDecode } from "jwt-decode";
+import { useSelector } from "react-redux";
+import { AxiosSecure } from "../../../../lib/AxiosSecure";
+import { API } from "../../../../api";
 
 const NewAccount = ({ setTabs }) => {
   const { refetchBankAccounts } = useGetAllBankAccount();
   const [addNewBank] = useBankMutation();
   const [isFormValid, setIsFormValid] = useState(false);
+  const [mobile, setMobile] = useState(null);
+  const { token } = useSelector((state) => state.auth);
+  const [orderId, setOrderId] = useState(null);
+  const [timer, setTimer] = useState(null);
   const [bankDetails, setBankDetails] = useState({
     accountName: "",
     ifsc: "",
     accountNumber: "",
     confirmAccountNumber: "",
     upiId: "",
+    otp: "",
   });
 
   /* Handle add bank function */
@@ -22,15 +31,26 @@ const NewAccount = ({ setTabs }) => {
     if (bankDetails.accountNumber !== bankDetails.confirmAccountNumber) {
       return toast.error("Account number not matched!");
     }
+
+    if (mobile && !bankDetails.otp) {
+      return toast.error("Please enter otp to add new account");
+    }
     /* generating random token for post data */
 
-    const bankData = {
+    let bankData = {
       accountName: bankDetails.accountName,
       ifsc: bankDetails.ifsc,
-      upiId: bankDetails.upiId,
       accountNumber: bankDetails.accountNumber,
+      upiId: bankDetails.upiId,
       type: "addBankAccount",
     };
+    if (mobile) {
+      bankData.mobile = mobile;
+      bankData.otp = bankDetails.otp;
+      bankData.orderId = orderId;
+    }
+
+    console.log(bankData);
 
     const res = await addNewBank(bankData).unwrap();
 
@@ -40,6 +60,7 @@ const NewAccount = ({ setTabs }) => {
         ifsc: "",
         accountNumber: "",
         confirmAccountNumber: "",
+        otp: "",
       });
       toast.success(res?.result?.message);
       setTabs("oldAccount");
@@ -53,14 +74,54 @@ const NewAccount = ({ setTabs }) => {
     const isaccountNameFilled = bankDetails.accountName.trim() !== "";
     const isaccountNumberFilled = bankDetails.accountNumber.trim() !== "";
     const isIfscFilled = bankDetails.ifsc.trim() !== "";
+    const isOTPFilled = mobile ? bankDetails.otp.trim() !== "" : true;
     const isFormValid =
-      isaccountNameFilled && isIfscFilled && isaccountNumberFilled;
+      isaccountNameFilled &&
+      isIfscFilled &&
+      isaccountNumberFilled &&
+      isOTPFilled;
     setIsFormValid(isFormValid);
   };
 
   useEffect(() => {
     validateForm(bankDetails);
   }, [bankDetails]);
+
+  const getOtp = async () => {
+    const otpData = {
+      mobile,
+    };
+
+    const res = await AxiosSecure.post(API.otp, otpData);
+    const data = res.data;
+    if (data?.success) {
+      setTimer(60);
+      setOrderId(data?.result?.orderId);
+      toast.success(data?.result?.message);
+    } else {
+      toast.error(data?.error?.errorMessage);
+    }
+  };
+
+  useEffect(() => {
+    const getMobile = () => {
+      const decode = jwtDecode(token);
+      if (decode?.mobile) {
+        setMobile(decode?.mobile);
+      }
+    };
+    getMobile();
+  }, [token]);
+
+  useEffect(() => {
+    if (timer > 0) {
+      setTimeout(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+    } else {
+      setTimer(null);
+    }
+  }, [timer]);
   return (
     <form
       onSubmit={handleAddBank}
@@ -91,31 +152,30 @@ const NewAccount = ({ setTabs }) => {
             <div className="text-xs ml-1 text-text_Primary"></div>
           </div>
         </div>
-        <div className="w-full relative h-full">
-          <div className="flex flex-col w-full">
-            <div className="ml-1 text-sm">
-              IFSC Code <span className="text-text_Primary">*</span>
-            </div>
-            <div className="relative">
-              <input
-                onChange={(e) => {
-                  setBankDetails({
-                    ...bankDetails,
-                    ifsc: e.target.value,
-                  });
-                }}
-                id="ifsc"
-                label="IFSC Code"
-                required
-                placeholder="Enter IFSC Code"
-                className="block w-full focus:outline-none py-2  border rounded-lg pl-4 pr-4 ml-0 mr-0"
-                type="text"
-                value={bankDetails.ifsc}
-              />
-            </div>
-            <div className="text-xs ml-1 text-text_Primary"></div>
+        <div className="flex flex-col w-full">
+          <div className="ml-1 text-sm">
+            Account Name <span className="text-text_Primary">*</span>
           </div>
+          <div className="relative">
+            <input
+              onChange={(e) => {
+                setBankDetails({
+                  ...bankDetails,
+                  accountName: e.target.value,
+                });
+              }}
+              id="accountName"
+              label="Account Name"
+              required
+              placeholder="Enter Account Name"
+              className="block w-full focus:outline-none py-2  border rounded-lg pl-4 pr-4 ml-0 mr-0"
+              type="text"
+              value={bankDetails.accountName}
+            />
+          </div>
+          <div className="text-xs ml-1 text-text_Primary"></div>
         </div>
+
         <div className="flex flex-col w-full">
           <div className="ml-1 text-sm">
             Account No <span className="text-text_Primary">*</span>
@@ -163,29 +223,101 @@ const NewAccount = ({ setTabs }) => {
           </div>
           <div className="text-xs ml-1 text-text_Primary"></div>
         </div>
-        <div className="flex flex-col w-full">
-          <div className="ml-1 text-sm">
-            Account Name <span className="text-text_Primary">*</span>
+        <div className="w-full relative h-full">
+          <div className="flex flex-col w-full">
+            <div className="ml-1 text-sm">
+              IFSC Code <span className="text-text_Primary">*</span>
+            </div>
+            <div className="relative">
+              <input
+                onChange={(e) => {
+                  setBankDetails({
+                    ...bankDetails,
+                    ifsc: e.target.value,
+                  });
+                }}
+                id="ifsc"
+                label="IFSC Code"
+                required
+                placeholder="Enter IFSC Code"
+                className="block w-full focus:outline-none py-2  border rounded-lg pl-4 pr-4 ml-0 mr-0"
+                type="text"
+                value={bankDetails.ifsc}
+              />
+            </div>
+            <div className="text-xs ml-1 text-text_Primary"></div>
           </div>
-          <div className="relative">
-            <input
-              onChange={(e) => {
-                setBankDetails({
-                  ...bankDetails,
-                  accountName: e.target.value,
-                });
-              }}
-              id="accountName"
-              label="Account Name"
-              required
-              placeholder="Enter Account Name"
-              className="block w-full focus:outline-none py-2  border rounded-lg pl-4 pr-4 ml-0 mr-0"
-              type="text"
-              value={bankDetails.accountName}
-            />
-          </div>
-          <div className="text-xs ml-1 text-text_Primary"></div>
         </div>
+        {mobile && (
+          <div className="flex flex-col gap-1 w-full">
+            <div title="passwordInput" className="w-full  ">
+              <div className="ml-1  lg:text-sm">
+                Mobile <span className="text-text_Primary">*</span>
+              </div>
+              <div className="flex w-full items-center py-2 bg-auth rounded-lg border">
+                {/* <span
+               id="dropdown-phone-button"
+               className="flex-shrink-0 z-10 inline-flex items-center pl-2 pr-1 text-sm sm:text-md font-normal text-center"
+             >
+               +91
+             </span> */}
+                <input
+                  readOnly
+                  id="mobile-no-input"
+                  className="px-2 block w-full focus:outline-none  font-lato bg-auth rounded-none text-text_Ternary pr-2 text-sm xs:text-md"
+                  placeholder="Phone Number"
+                  type="text"
+                  value={mobile}
+                />
+
+                <div className="w-max">
+                  {timer ? (
+                    <button
+                      onClick={getOtp}
+                      className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out font-lato-bold h-fit bg-bg_Primary text-text_Quaternary transition-all ease-in-out text-xs whitespace-nowrap mr-1 py-1 px-3 rounded active:scale-[0.98] active:opacity-95 disabled:bg-bg_Slate500 disabled:opacity-50 font-medium relative flex items-center justify-center cursor-pointer"
+                      type="button"
+                    >
+                      <span className=" ">Retry in {timer}</span>
+                      {/* <span className="shimmer"></span> */}
+                    </button>
+                  ) : (
+                    <button
+                      onClick={getOtp}
+                      className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out font-lato-bold h-fit bg-bg_Primary text-text_Quaternary transition-all ease-in-out text-xs whitespace-nowrap mr-1 py-1 px-3 rounded active:scale-[0.98] active:opacity-95 disabled:bg-bg_Slate500 disabled:opacity-50 font-medium relative flex items-center justify-center cursor-pointer"
+                      type="button"
+                    >
+                      <span className=" ">Get OTP</span>
+                      <span className="shimmer"></span>
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {mobile && (
+          <div className="flex flex-col gap-1 w-full">
+            <div title="passwordInput" className="w-full  uppercase">
+              <div className=" ml-1  lg:text-sm">
+                OTP <span className="text-text_Primary">*</span>
+              </div>
+              <div className="flex w-full items-center border p-1 bg-auth rounded-lg mt-2">
+                <input
+                  onChange={(e) => {
+                    setBankDetails({
+                      ...bankDetails,
+                      otp: e.target.value,
+                    });
+                  }}
+                  id="otpSignUp"
+                  className="block w-full focus:outline-none w-full font-lato rounded-none py-1 text-text_Ternary px-2 text-sm xs:text-md bg-auth"
+                  placeholder="Enter OTP"
+                  type="text"
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </div>
 
       <div className="flex items-start justify-center gap-x-2">
