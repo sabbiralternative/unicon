4a9c53558281b4506db3a375dc527bdcaec55787
sabbirHossain <sabbirshnt@gmail.com>
2025-06-10 12:03:13 +0600

    done

--- src/components/shared/SuspendedOdd/SuspendedOdd.jsx
+++ src/components/shared/SuspendedOdd/SuspendedOdd.jsx
@@ -2,8 +2,7 @@ const SuspendedOdd = ({ colSpan }) => {
   // bg-bg_ballRunning
   return (
     <span
-      className={`col-span-${colSpan} h-12 lg:col-span-7 w-full overflow-auto border-t border-borderColorOfMarket">
-      <span className="w-full flex items-center px-[1px] md:px-[3px] justify-center h-full cursor-not-allowed`}
+      className={`col-span-${colSpan} h-12 lg:col-span-7 w-full overflow-auto border-t border-borderColorOfMarket`}
     >
       <span className="text-xs text-center w-full h-full flex items-center bg-bg_ballRunning  text-text_SuspendedFontColor font-bold justify-center rounded-sm">
         SUSPENDED
