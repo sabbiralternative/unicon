48ec6f1606aa22ec2925f1677b41d90a44538d6f
sabbirHossain <sabbirshnt@gmail.com>
2025-06-10 11:52:33 +0600

    done

--- src/components/ui/IFrame/IFrame.jsx
+++ src/components/ui/IFrame/IFrame.jsx
@@ -1,7 +1,7 @@
 const IFrameScore = ({ score, betType, setBetType, iFrame }) => {
   return (
     <>
-      {betType === "video" || betType === "tracker" ? (
+      {betType === "video" && (
         <div className=" col-span-1 ">
           <div className=" relative w-full h-auto overflow-hidden bg-transparent ">
             <div
@@ -37,17 +37,6 @@ const IFrameScore = ({ score, betType, setBetType, iFrame }) => {
                   </g>
                 </svg>
               </div>
-              {score && score?.tracker !== null && betType === "tracker" && (
-                <div className="w-full overflow-hidden h-[125px]">
-                  <iframe
-                    id="videoComponent"
-                    className="w-full h-auto relative overflow-hidden   bg-transparent"
-                    src={iFrame}
-                    width="100%"
-                    allowfullscreen=""
-                  ></iframe>
-                </div>
-              )}
 
               {score && iFrame && betType === "video" && score?.hasVideo && (
                 <iframe
@@ -62,7 +51,29 @@ const IFrameScore = ({ score, betType, setBetType, iFrame }) => {
             {/* <div className=" absolute top-0 w-full max-h-[309px] sm:max-h-[144px] lg:max-h-[309px]  overflow-hidden h-[55vw] md:h-[58vw] bg-transparent z-0"></div> */}
           </div>
         </div>
-      ) : null}
+      )}
+
+      <div className=" col-span-1 ">
+        <div className=" relative w-full h-auto overflow-hidden bg-transparent ">
+          <div
+            draggable="false"
+            className="
+                      w-full flex items-center justify-center overflow-y-auto bg-transparent relative z-10 transition-all ease-in-out duration-100"
+          >
+            {score && score?.tracker !== null && (
+              <div className="w-full overflow-hidden h-[125px]">
+                <iframe
+                  id="videoComponent"
+                  className="w-full h-auto relative overflow-hidden   bg-transparent"
+                  src={score?.tracker}
+                  width="100%"
+                  allowfullscreen=""
+                ></iframe>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
