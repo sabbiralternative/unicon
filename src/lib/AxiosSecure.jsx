d97f11a7b840451fcd6c35ce237108da71600192
sabbirHossain <sabbirshnt@gmail.com>
2025-06-23 18:36:31 +0600

    done

--- src/lib/AxiosSecure.jsx
+++ src/lib/AxiosSecure.jsx
@@ -26,8 +26,6 @@ AxiosSecure.interceptors.request.use(
         payload.language = localStorage.getItem("language") || "english";
       }
 
-      console.log(payload);
-
       const encryptedData = handleEncryptData(payload);
       config.data = encryptedData;
     }
