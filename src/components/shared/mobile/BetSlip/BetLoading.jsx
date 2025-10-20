7e7b5d4fb9e3feadd4773c14f33eaf127a23b4af
sabbirHossain <sabbirshnt@gmail.com>
2025-10-20 11:46:35 +0600

    done

--- src/components/shared/mobile/BetSlip/BetLoading.jsx
+++ src/components/shared/mobile/BetSlip/BetLoading.jsx
@@ -21,15 +21,14 @@ const BetLoading = ({ setBetDelay, betDelay, absolute }) => {
       <div
         className={`z-2 popUpBoxShadow popUpOpenAnimation absolute w-[86%] max-w-[380px] py-3 px-2 rounded-[10px] bg-bg_Quaternary p-2 xs:p-5`}
       >
-        <div className="flex flex-col gap-4 align-top items-center">
-          {/* <span className="relative w-max min-w-6 min-h-6 flex items-center justify-center p-2 pt-6">
-            {betDelay > 0 && (
-              <span className="absolute min-w-14 min-h-14 animate-spin rounded-full border-[2px] border-primary border-dashed"></span>
-            )}
-            <span className="font-semibold text-text_Ternary">
+        <div className="flex flex-col gap-4 align-top items-center pt-2">
+          <span className="relative w-max min-w-6 min-h-6 flex items-center justify-center p-2 pt-6">
+            <span className="absolute min-w-8 min-h-8 animate-spin rounded-full border-[2px] border-primary border-dashed"></span>
+
+            {/* <span className="font-semibold text-text_Ternary">
               {betDelay > 0 && betDelay}
-            </span>
-          </span> */}
+            </span> */}
+          </span>
           <span className="font-semibold mt-[4px]">
             Your bet is being processed...
           </span>
