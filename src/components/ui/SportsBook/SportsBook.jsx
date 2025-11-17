f93c7f87875a850aed5fbeade827b017cc10781e
sabbirHossain <sabbirshnt@gmail.com>
2025-11-17 16:36:43 +0600

    done

--- src/components/ui/SportsBook/SportsBook.jsx
+++ src/components/ui/SportsBook/SportsBook.jsx
@@ -5,8 +5,7 @@ import ColumnThree from "./ColumnThree";
 import ColumnTwo from "./ColumnTwo";
 import ColumnOne from "./ColumnOne";
 import BetSlip from "../../shared/mobile/BetSlip/BetSlip";
-// .a23_css .bt12695
-// .a23_css .bt12689
+
 const SportsBook = ({ sportsBook }) => {
   const { eventTypeId } = useParams();
   const [priceClasses, setPriceClasses] = useState({});
@@ -44,24 +43,23 @@ const SportsBook = ({ sportsBook }) => {
 
   return (
     <>
-      {eventTypeId != 4 &&
-        sports?.map((group) =>
-          group?.Items?.map((item, iIdx) => {
-            const isOpen = openItems[iIdx];
+      {sports?.map((group) =>
+        group?.Items?.map((item, iIdx) => {
+          const isOpen = openItems[iIdx];
 
-            return (
-              <div key={iIdx} style={{ marginBottom: "10px" }}>
-                <div className="a23_css bt12687">
-                  <div onClick={() => toggleItem(iIdx)} className="bt12695">
-                    <div className="bt12689 " data-editor-id="marketTitle">
-                      {item?.Name}
-                      <div
-                        className="bt6471 bt12696 bt12690"
-                        style={{ width: "16px", height: "16px" }}
-                      >
-                        {isOpen ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
+          return (
+            <div key={iIdx} style={{ marginBottom: "10px" }}>
+              <div className="a23_css bt12687">
+                <div onClick={() => toggleItem(iIdx)} className="bt12695">
+                  <div className="bt12689 " data-editor-id="marketTitle">
+                    {item?.Name}
+                    <div
+                      className="bt6471 bt12696 bt12690"
+                      style={{ width: "16px", height: "16px" }}
+                    >
+                      {isOpen ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
 
-                        {/* <svg
+                      {/* <svg
                         width="16"
                         height="16"
                         viewBox="0 0 16 16"
@@ -69,55 +67,53 @@ const SportsBook = ({ sportsBook }) => {
                       >
                         <path d="M8.7542 11.1529C8.35634 11.6157 7.64366 11.6157 7.2458 11.1529L4.24545 7.66298C3.68586 7.01207 4.14485 6 4.99964 6L11.0004 6C11.8551 6 12.3141 7.01207 11.7546 7.66298L8.7542 11.1529Z"></path>
                       </svg> */}
-                      </div>
                     </div>
                   </div>
-                  {item?.MColumnCount === 3 && (
-                    <ColumnThree
-                      setRunnerId={setRunnerId}
-                      item={item}
-                      isOpen={isOpen}
-                      sportsBook={sportsBook}
-                      priceClasses={priceClasses}
-                      setPriceClasses={setPriceClasses}
-                      prevPrices={prevPrices}
-                      setPrevPrices={setPrevPrices}
-                    />
-                  )}
-                  {item?.MColumnCount === 2 && (
-                    <ColumnTwo
-                      setRunnerId={setRunnerId}
-                      item={item}
-                      isOpen={isOpen}
-                      sportsBook={sportsBook}
-                      priceClasses={priceClasses}
-                      setPriceClasses={setPriceClasses}
-                      prevPrices={prevPrices}
-                      setPrevPrices={setPrevPrices}
-                    />
-                  )}
-                  {item?.MColumnCount === 1 && (
-                    <ColumnOne
-                      setRunnerId={setRunnerId}
-                      item={item}
-                      isOpen={isOpen}
-                      sportsBook={sportsBook}
-                      priceClasses={priceClasses}
-                      setPriceClasses={setPriceClasses}
-                      prevPrices={prevPrices}
-                      setPrevPrices={setPrevPrices}
-                    />
-                  )}
                 </div>
-                <div className="block lg:hidden">
-                  {item?.Id === runnerId && (
-                    <BetSlip setRunnerId={setRunnerId} />
-                  )}
-                </div>
+                {item?.MColumnCount === 3 && (
+                  <ColumnThree
+                    setRunnerId={setRunnerId}
+                    item={item}
+                    isOpen={isOpen}
+                    sportsBook={sportsBook}
+                    priceClasses={priceClasses}
+                    setPriceClasses={setPriceClasses}
+                    prevPrices={prevPrices}
+                    setPrevPrices={setPrevPrices}
+                  />
+                )}
+                {item?.MColumnCount === 2 && (
+                  <ColumnTwo
+                    setRunnerId={setRunnerId}
+                    item={item}
+                    isOpen={isOpen}
+                    sportsBook={sportsBook}
+                    priceClasses={priceClasses}
+                    setPriceClasses={setPriceClasses}
+                    prevPrices={prevPrices}
+                    setPrevPrices={setPrevPrices}
+                  />
+                )}
+                {item?.MColumnCount === 1 && (
+                  <ColumnOne
+                    setRunnerId={setRunnerId}
+                    item={item}
+                    isOpen={isOpen}
+                    sportsBook={sportsBook}
+                    priceClasses={priceClasses}
+                    setPriceClasses={setPriceClasses}
+                    prevPrices={prevPrices}
+                    setPrevPrices={setPrevPrices}
+                  />
+                )}
               </div>
-            );
-          })
-        )}
+              <div className="block lg:hidden">
+                {item?.Id === runnerId && <BetSlip setRunnerId={setRunnerId} />}
+              </div>
+            </div>
+          );
+        })
+      )}
     </>
   );
 };
