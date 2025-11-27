54135bd45f14649a8c4984eff6f9ef559c35904d
sabbirHossain <sabbirshnt@gmail.com>
2025-11-27 12:02:08 +0600

    done

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,8 +3,10 @@ import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { settings } from "../../api";
 import { useDispatch } from "react-redux";
 import { setShowRegisterModal } from "../../redux/features/stateSlice";
+import { useIndex } from "../../hooks";
 
 const NotFound = () => {
+  const { mutate } = useIndex();
   const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -21,6 +23,7 @@ const NotFound = () => {
         const splitPath = location.pathname.split("/");
         const lastDigit = splitPath[splitPath?.length - 1];
         if (parseFloat(lastDigit)) {
+          mutate({ type: "addReferralCount", referral_id: lastDigit });
           localStorage.setItem("referralCode", lastDigit);
           dispatch(setShowRegisterModal(true));
           navigate("/");
