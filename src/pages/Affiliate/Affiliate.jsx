80aa9841b75069006bd6a81d9a0e97e3a3100d67
sabbirHossain <sabbirshnt@gmail.com>
2025-12-01 17:58:30 +0600

    done

--- src/pages/Affiliate/Affiliate.jsx
+++ src/pages/Affiliate/Affiliate.jsx
@@ -6,8 +6,14 @@ import InviteSection from "../../components/modules/Affiliate/InviteSection";
 import TopFiveLossUser from "../../components/modules/Affiliate/TopFiveLossUser";
 import BonusInformation from "../../components/modules/Affiliate/BonusInformation";
 import TodayProfitLoss from "../../components/modules/Affiliate/TodayProfitLoss";
+import { Fragment, useState } from "react";
+import UserList from "../../components/modules/Affiliate/UserList";
+import ProfitLoss from "../../components/modules/Affiliate/ProfitLoss";
+import Reports from "../../components/modules/Affiliate/Reports";
+import Footer from "../../components/modules/Affiliate/Footer";
 
 const Affiliate = () => {
+  const [tab, setTab] = useState("dashboard");
   return (
     <div className="flex flex-col transition-all">
       <div className="flex items-start flex-col lg:flex-row justify-start w-full lg:px-12 xl:px-20 xlg:px-24">
@@ -23,11 +29,31 @@ const Affiliate = () => {
               className="container"
               style={{ maxWidth: "100%" }}
             >
-              <TodayStatusSection />
-              <InviteSection />
-              <TopFiveLossUser />
-              <BonusInformation />
-              <TodayProfitLoss />
+              {tab === "dashboard" && (
+                <Fragment>
+                  <TodayStatusSection />
+                  <InviteSection />
+                  <TopFiveLossUser />
+                  <BonusInformation />
+                  <TodayProfitLoss />
+                </Fragment>
+              )}
+              {tab === "user-list" && (
+                <div data-v-4c49d924 className="">
+                  <UserList />
+                </div>
+              )}
+              {tab === "pnl" && (
+                <div data-v-4c49d924 className="">
+                  <ProfitLoss />
+                </div>
+              )}
+              {tab === "reports" && (
+                <div data-v-4c49d924 className="">
+                  <Reports />
+                </div>
+              )}
+              <Footer setTab={setTab} tab={tab} />
             </div>
           </div>
         </div>
