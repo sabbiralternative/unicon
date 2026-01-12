b73c4ee34ea9e3f21375b9f4ac61a66c70c60d46
sabbirHossain <sabbirshnt@gmail.com>
2026-01-12 16:37:53 +0600

    done

--- src/utils/handleRandomToken.js
+++ src/utils/handleRandomToken.js
@@ -27,9 +27,9 @@ const handleRandomToken = () => {
   const utcTimestamp = Math.floor(Date.now() / 1000);
 
   const multipliedTimestamp = utcTimestamp * 247;
-  const randomSixDigitNumber = Math.floor(100000 + Math.random() * 900000);
+  const randomSixDigitNumber_1 = Math.floor(100000 + Math.random() * 900000);
+  const randomSixDigitNumber_2 = Math.floor(100000 + Math.random() * 900000);
 
-  return `${randomSixDigitNumber}${multipliedTimestamp}`;
+  return `${randomSixDigitNumber_1}${multipliedTimestamp}${randomSixDigitNumber_2}`;
 };
-
 export default handleRandomToken;
